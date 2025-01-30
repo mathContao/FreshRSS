7d6b93317847ed0020a85d8d1409ece404ecd1d9
math-gh <1645099+math-GH@users.noreply.github.com>
2025-01-30 23:24:38 +0100

    Über -> info

--- app/i18n/de/gen.php
+++ app/i18n/de/gen.php
@@ -176,7 +176,7 @@ return array(
 		'zh-tw' => '正體中文',	// IGNORE
 	),
 	'menu' => array(
-		'about' => 'Über',
+		'about' => 'Info',
 		'account' => 'Account',	// IGNORE
 		'admin' => 'Administration',	// IGNORE
 		'archiving' => 'Archivierung',
